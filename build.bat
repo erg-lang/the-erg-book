5f47ba43c6f13ba8bda85619a0ccd792aee6f69f
Shunsuke Shibayama <sbym1346@gmail.com>
2022-09-19 23:58:03 +0900

    multilingualization

--- build.bat
+++ build.bat
@@ -2,7 +2,37 @@
 
 svn export https://github.com/erg-lang/erg/trunk/doc/EN/syntax/
 
-mdbook build
+mdbook build --dest-dir docs/EN
+
+python replace_links.py
+
+timeout 1
+
+rmdir /s /q syntax
+
+svn export https://github.com/erg-lang/erg/trunk/doc/JA/syntax/
+
+mdbook build --dest-dir docs/JA
+
+python replace_links.py
+
+timeout 1
+
+rmdir /s /q syntax
+
+svn export https://github.com/erg-lang/erg/trunk/doc/zh_CN/syntax/
+
+mdbook build --dest-dir docs/zh_CN
+
+python replace_links.py
+
+timeout 1
+
+rmdir /s /q syntax
+
+svn export https://github.com/erg-lang/erg/trunk/doc/zh_TW/syntax/
+
+mdbook build --dest-dir docs/zh_TW
 
 python replace_links.py
 
