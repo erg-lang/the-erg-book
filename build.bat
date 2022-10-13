6903d2099db560f44dd37a22cd2cc93992cbf86a
Shunsuke Shibayama <sbym1346@gmail.com>
2022-10-13 14:26:30 +0900

    Add replace_links.er

--- build.bat
+++ build.bat
@@ -1,43 +1,30 @@
 @echo off
 
-svn export https://github.com/erg-lang/erg/trunk/doc/EN/syntax/
-
-mdbook build --dest-dir docs/EN
-
-python replace_links.py
-
-timeout 1
-
-rmdir /s /q syntax
-
+rmdir /s /q docs
+mkdir docs
 svn export https://github.com/erg-lang/erg/trunk/doc/JA/syntax/
-
-mdbook build --dest-dir docs/JA
-
+mdbook build --dest-dir tmp
 python replace_links.py
-
+move tmp docs\JA
 timeout 1
-
 rmdir /s /q syntax
-
 svn export https://github.com/erg-lang/erg/trunk/doc/zh_CN/syntax/
-
-mdbook build --dest-dir docs/zh_CN
-
+mdbook build --dest-dir tmp
 python replace_links.py
-
+move tmp docs\zh_CN
 timeout 1
-
 rmdir /s /q syntax
-
 svn export https://github.com/erg-lang/erg/trunk/doc/zh_TW/syntax/
-
-mdbook build --dest-dir docs/zh_TW
-
+mdbook build --dest-dir tmp
 python replace_links.py
-
+move tmp docs\zh_TW
 timeout 1
-
 rmdir /s /q syntax
-
+svn export https://github.com/erg-lang/erg/trunk/doc/EN/syntax/
+mdbook build --dest-dir tmp
+python replace_links.py
+move tmp\* docs\
+timeout 1
+rmdir /s /q syntax
+rmdir /s /q tmp
 echo done
